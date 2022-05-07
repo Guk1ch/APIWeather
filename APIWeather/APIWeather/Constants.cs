589a67b35a4c8bcac39343fdbdd331f0e20e38c3
Егор Артемьев <batman_batman_1910@mail.ru>
2022-05-07 23:22:37 +0300

    add gitignore

--- APIWeather/APIWeather/Constants.cs
+++ APIWeather/APIWeather/Constants.cs
@@ -1,10 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
 using System.Text;
+
 
 namespace APIWeather
 {
     public class Constants
     {
+        public static string RestUrl = "https://api.openweathermap.org/data/2.5/weather";
+        public static string GetApiKey()
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            var resourceName = "TestApiApp.ApiKey.txt";
+
+            using (Stream stream = assembly.GetManifestResourceStream(resourceName))
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                string result = reader.ReadToEnd();
+                return result;
+            }
+        }
     }
 }
